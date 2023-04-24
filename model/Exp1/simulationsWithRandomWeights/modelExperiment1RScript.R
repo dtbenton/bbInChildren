d62ed14cccf5d69c3134989aa5d20c490499263f
dtbenton <deon.benton@vanderbilt.edu>
2023-04-24 16:39:23 -0500

    s

--- model/Exp1/simulationsWithRandomWeights/modelExperiment1RScript.R
+++ model/Exp1/simulationsWithRandomWeights/modelExperiment1RScript.R
@@ -26,99 +26,99 @@ D = read.csv(file.choose(), header = TRUE, stringsAsFactors = FALSE)
 # get names of columns
 names(D)
 
+# get structure of data
+str(D)
+
+# convert variables
+D$trial = factor(D$trial)
+D$phase = factor(D$phase)
+D$object = factor(D$object)
+D$choice = as.numeric(D$choice)
+
+# change level names of D$trial
+levels(D$trial) = c("control","experimental")
+levels(D$trial)
+
 # BB trial objects #
-# main
-BB.A.main = D$choice[D$object=="A" & D$phase=="Phase 1" & D$trial=="main" & D$condition=="BB"]+
-  D$choice[D$object=="A" & D$phase=="Phase 2" & D$trial=="main" & D$condition=="BB"]
+# experimental
+BB.A.experimental = D$choice[D$object=="A" & D$trial=="experimental" & D$condition=="BB"]
 
-BB.B.main = D$choice[D$object=="B" & D$phase=="Phase 1" & D$trial=="main" & D$condition=="BB"]+
-  D$choice[D$object=="B" & D$phase=="Phase 2" & D$trial=="main" & D$condition=="BB"]
+BB.B.experimental = D$choice[D$object=="B" & D$trial=="experimental" & D$condition=="BB"]
 
-BB.C.main = D$choice[D$object=="C" & D$phase=="Phase 1" & D$trial=="main" & D$condition=="BB"]+
-  D$choice[D$object=="C" & D$phase=="Phase 2" & D$trial=="main" & D$condition=="BB"]
+BB.C.experimental = D$choice[D$object=="C" & D$trial=="experimental" & D$condition=="BB"]
 
 
 # control
-BB.A.control = D$choice[D$object=="A" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="BB"]+
-  D$choice[D$object=="A" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="BB"]
+BB.A.control = D$choice[D$object=="A" & D$trial=="control" & D$condition=="BB"]
 
-BB.B.control = D$choice[D$object=="B" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="BB"]+
-  D$choice[D$object=="B" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="BB"]
+BB.B.control = D$choice[D$object=="B" & D$trial=="control" & D$condition=="BB"]
 
-BB.C.control = D$choice[D$object=="C" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="BB"]+
-  D$choice[D$object=="C" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="BB"]
+BB.C.control = D$choice[D$object=="C" & D$trial=="control" & D$condition=="BB"]
 
-BB.D.control = D$choice[D$object=="D" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="BB"]+
-  D$choice[D$object=="D" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="BB"]
+BB.D.control = D$choice[D$object=="D" & D$trial=="control" & D$condition=="BB"]
 
 
 
 ## ISO trial objects ##
-# main
-ISO.A.main = D$choice[D$object=="A" & D$phase=="Phase 1" & D$trial=="main" & D$condition=="ISO"]+
-  D$choice[D$object=="A" & D$phase=="Phase 2" & D$trial=="main" & D$condition=="ISO"]
+# experimental
+ISO.A.experimental = D$choice[D$object=="A" & D$trial=="experimental" & D$condition=="ISO"]
 
-ISO.B.main = D$choice[D$object=="B" & D$phase=="Phase 1" & D$trial=="main" & D$condition=="ISO"]+
-  D$choice[D$object=="B" & D$phase=="Phase 2" & D$trial=="main" & D$condition=="ISO"]
+ISO.B.experimental = D$choice[D$object=="B" & D$trial=="experimental" & D$condition=="ISO"]
 
-ISO.C.main = D$choice[D$object=="C" & D$phase=="Phase 1" & D$trial=="main" & D$condition=="ISO"]+
-  D$choice[D$object=="C" & D$phase=="Phase 2" & D$trial=="main" & D$condition=="ISO"]
+ISO.C.experimental = D$choice[D$object=="C" & D$trial=="experimental" & D$condition=="ISO"]
 
 
 # control
-ISO.A.control = D$choice[D$object=="A" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="ISO"]+
-  D$choice[D$object=="A" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="ISO"]
+ISO.A.control = D$choice[D$object=="A" & D$trial=="control" & D$condition=="ISO"]
 
-ISO.B.control = D$choice[D$object=="B" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="ISO"]+
-  D$choice[D$object=="B" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="ISO"]
+ISO.B.control = D$choice[D$object=="B" & D$trial=="control" & D$condition=="ISO"]
 
-ISO.C.control = D$choice[D$object=="C" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="ISO"]+
-  D$choice[D$object=="C" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="ISO"]
+ISO.C.control = D$choice[D$object=="C" & D$trial=="control" & D$condition=="ISO"]
 
-ISO.D.control = D$choice[D$object=="D" & D$phase=="Phase 1" & D$trial=="control" & D$condition=="ISO"]+
-  D$choice[D$object=="D" & D$phase=="Phase 2" & D$trial=="control" & D$condition=="ISO"]
+ISO.D.control = D$choice[D$object=="D" & D$trial=="control" & D$condition=="ISO"]
+
+## BB AND ISO EXPERIMENTAL
 
 # create new data frame from new columns 
-D.new.main = data.frame(BB.A.main = BB.A.main, BB.B.main = BB.B.main,
-                        BB.C.main = BB.C.main, ISO.A.main = ISO.A.main, 
-                        ISO.B.main = ISO.B.main, ISO.C.main = ISO.C.main)
+D.new.experimental = data.frame(BB.A.experimental = BB.A.experimental, BB.B.experimental = BB.B.experimental,
+                        BB.C.experimental = BB.C.experimental, ISO.A.experimental = ISO.A.experimental, 
+                        ISO.B.experimental = ISO.B.experimental, ISO.C.experimental = ISO.C.experimental)
 
-D_main_tall =  reshape(D.new.main, varying = c(1:6), v.names = "measure", 
+D_experimental_tall =  reshape(D.new.experimental, varying = c(1:6), v.names = "measure", 
                        timevar = "condition",   direction = "long")
-D_main_tall$ID = rep(c(1:16), times = 6)
+D_experimental_tall$ID = rep(c(1:200), times = 6)
 
 # remove 'id' column from the reshape() function
-D_main_tall$id = NULL
+D_experimental_tall$id = NULL
 
 # organize the dataframe by ID
-D_main_tall = D_main_tall[order(D_main_tall$ID),] 
+D_experimental_tall = D_experimental_tall[order(D_experimental_tall$ID),] 
 
 # replace the 'condition' column with a more appropriate one
-D_main_tall$condition = rep(c("BB","ISO"), each = 3, times = 16)
+D_experimental_tall$condition = rep(c("BB","ISO"), each = 3, times = 200)
 
 # create a 'trial' column
-D_main_tall$trial = rep(c("main"), times = 96)
+D_experimental_tall$trial = rep(c("experimental"), times = 1200)
 
 # create 'object' column
-D_main_tall$objects = rep(c("A","B","C"), times = 32)
+D_experimental_tall$objects = rep(c("A","B","C"), times = 400)
 
 # remove 'row.names' column 
-D_main_tall$row.names = NULL
+D_experimental_tall$row.names = NULL
 
 # reorder columns 
-D_main_tall = D_main_tall[,c(3,4,1,5,2)]
+D_experimental_tall = D_experimental_tall[,c(3,4,1,5,2)]
 
+## BB AND ISO CONTROL
 
+# create new data frame from new columns 
+D.new.control = data.frame(BB.A.control = BB.A.control, BB.B.control = BB.B.control,
+                           BB.C.control = BB.C.control, ISO.A.control = ISO.A.control, 
+                           ISO.B.control = ISO.B.control, ISO.C.control = ISO.C.control)
 
-D.new.control = data.frame(BB.A.control = BB.A.control,
-                           BB.B.control = BB.B.control, BB.C.control = BB.C.control,
-                           BB.D.control = BB.D.control, ISO.A.control = ISO.A.control,
-                           ISO.B.control = ISO.B.control, ISO.C.control = ISO.C.control,
-                           ISO.D.control = ISO.D.control)
-
-D_control_tall =  reshape(D.new.control, varying = c(1:8), v.names = "measure", 
+D_control_tall =  reshape(D.new.control, varying = c(1:6), v.names = "measure", 
                           timevar = "condition",   direction = "long")
-D_control_tall$ID = rep(c(1:16), times = 8)
+D_control_tall$ID = rep(c(1:200), times = 6)
 
 # remove 'id' column from the reshape() function
 D_control_tall$id = NULL
@@ -127,13 +127,13 @@ D_control_tall$id = NULL
 D_control_tall = D_control_tall[order(D_control_tall$ID),] 
 
 # replace the 'condition' column with a more appropriate one
-D_control_tall$condition = rep(c("BB","ISO"), each = 4, times = 16)
+D_control_tall$condition = rep(c("BB","ISO"), each = 3, times = 200)
 
 # create a 'trial' column
-D_control_tall$trial = rep(c("control"), times = 128)
+D_control_tall$trial = rep(c("control"), times = 1200)
 
 # create 'object' column
-D_control_tall$objects = rep(c("A","B","C","D"), times = 32)
+D_control_tall$objects = rep(c("A","B","C"), times = 400)
 
 # remove 'row.names' column 
 D_control_tall$row.names = NULL
@@ -142,8 +142,9 @@ D_control_tall$row.names = NULL
 D_control_tall = D_control_tall[,c(3,4,1,5,2)]
 
 
+
 # combine the dataframes
-D_tall = rbind(D_main_tall,
+D_tall = rbind(D_experimental_tall,
                D_control_tall)
 fix(D_tall)
 names(D_tall)
@@ -160,6 +161,7 @@ condition_barplot + stat_summary(fun = mean, geom = "bar", position = "dodge") +
                              "#696969",
                              "#548548")) +
   
-  coord_cartesian(ylim=c(0, 2.2)) +
+  coord_cartesian(ylim=c(0, 0.95)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        axis.title.x=element_blank())
